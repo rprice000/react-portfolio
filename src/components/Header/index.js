e288a7b31e1355540758aa9ffbd71ab0ad339269
rprice000 <53877937+rprice000@users.noreply.github.com>
2022-01-07 10:04:47 -0600

    setup react portfolio project

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+       <header>
+           <h1>Reagan's React Web Portfolio</h1>
+       </header> 
+    )
+}
+
+
+export default Header;
