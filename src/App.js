8179ad318396a62eed72857635cec868c7f6bd07
rprice000 <53877937+rprice000@users.noreply.github.com>
2022-01-07 10:32:59 -0600

    setup header and nav components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Header from './components/Header'
+import Header from './components/Header';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Nav></Nav>
     </div>
   );
 }
---
+++ src/components/Nav/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+
+function Nav() {
+    return (
+        <nav>
+        <ul>
+
+          <li>
+            
+            Home
+          
+          </li>
+
+          <li>
+            
+              About Me
+            
+          </li>
+
+          <li>
+            
+              My Projects
+            
+          </li>
+
+          <li>
+            
+              Contact Me
+            
+          </li>
+
+        </ul>
+      </nav>
+    )
+}
+
+export default Nav;
